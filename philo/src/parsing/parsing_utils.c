b0f50fa2796d711a84ca01647f627eaec78df331
gabo310599 <gabrielojedasanchez@gmail.com>
2026-02-14 13:53:36 +0100

    fixed conflict with version 1.0 (old version)

--- philo/src/parsing/parsing_utils.c
+++ philo/src/parsing/parsing_utils.c
@@ -5,19 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gojeda <gojeda@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD:philo/src/parser.c
-/*   Created: 2025/07/31 11:46:35 by gojeda            #+#    #+#             */
-/*   Updated: 2025/08/07 20:28:42 by gojeda           ###   ########.fr       */
-=======
 /*   Created: 2026/02/14 12:20:02 by gojeda            #+#    #+#             */
-/*   Updated: 2026/02/14 12:31:46 by gojeda           ###   ########.fr       */
->>>>>>> c4889b8 (philos2.0 (new version, needs testing and readme)):philo/src/parsing/parsing_utils.c
+/*   Updated: 2026/02/14 13:52:21 by gojeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-//Verificamos que un string sea un numero
+// Verificamos que un string sea un numero
 int	ft_is_number(char *str)
 {
 	int	i;
@@ -34,7 +29,7 @@ int	ft_is_number(char *str)
 	return (1);
 }
 
-//Atol para parseo seguro
+// Atol para parseo seguro
 long	ft_atol_safe(const char *str)
 {
 	long	result;
@@ -49,21 +44,5 @@ long	ft_atol_safe(const char *str)
 			return (-1);
 		i++;
 	}
-<<<<<<< HEAD:philo/src/parser.c
-	rules->philo_count = ft_atoi(argv[1]);
-	rules->time_to_die = ft_atoi(argv[2]);
-	rules->time_to_eat = ft_atoi(argv[3]);
-	rules->time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-	{
-		rules->must_eat_count = ft_atoi(argv[5]);
-		if (rules->must_eat_count <= 0)
-			return ((print_error("must_eat_count debe ser > 0"), 0));
-	}
-	else
-		rules->must_eat_count = -1;
-	return (rules->someone_died = 0, rules->start_time = get_time_in_ms(), 1);
-=======
 	return (result);
->>>>>>> c4889b8 (philos2.0 (new version, needs testing and readme)):philo/src/parsing/parsing_utils.c
 }
